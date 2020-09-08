eb915cbc82595f881af7d7bef4443e9bb1631443
julien-vidal <julien.vidal@elium.io>
2020-09-08 12:06:31 +0200

    Add enable/DisableAll keys link

--- src/display-state.ts
+++ src/display-state.ts
@@ -1,9 +1,44 @@
-import { isKeyEnabled, setEnabledKey } from './config'
+import {
+  disableAllKeys,
+  enableAllKeys,
+  isKeyEnabled,
+  setEnabledKey,
+  SwitchActionKeys,
+  switchActionsKeysArray
+} from './config'
 
 let previousDisplayedState = {}
 
+const displayEnableAllLink = (getExtendedState) => {
+  return {
+    [SwitchActionKeys.EnableAll]: {
+      enabled: '',
+      get ['Click on "(…)" to toggle']() {
+        enableAllKeys()
+        displayState(getExtendedState())
+        return 'All keys have been enabled'
+      }
+    }
+  }
+}
+
+const displayDisableAllLink = (state, getExtendedState) => {
+  return {
+    [SwitchActionKeys.DisableAll]: {
+      enabled: '',
+      get ['Click on "(…)" to toggle']() {
+        disableAllKeys(Object.keys(state))
+        displayState(getExtendedState())
+        return 'All keys have been disabled'
+      }
+    }
+  }
+}
+
 export const displayState = state => {
-  const extendedState = Object.keys(state).reduce(
+  const extendedState = Object.keys(state)
+    .filter((item) => !switchActionsKeysArray.includes(item))
+    .reduce(
     (prev, curr) => ({
       ...prev,
       [curr]: {
@@ -16,7 +51,10 @@ export const displayState = state => {
         }
       }
     }),
-    {}
+    {
+      ...displayEnableAllLink(() => extendedState),
+      ...displayDisableAllLink(state, () => extendedState)
+    }
   )
   const identical = Object.keys(extendedState).reduce(
     (prev, curr) => prev && extendedState[curr].enabled === previousDisplayedState[curr]?.enabled,
